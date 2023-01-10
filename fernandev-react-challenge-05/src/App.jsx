d4e8e62b931ef83fe11351304cacd958f3a52ded
Otto Neto <ottoneto@MacBook-Pro-de-Otto.local>
2023-01-09 21:26:19 -0300

    desafio 4

---
+++ fernandev-react-challenge-05/src/App.jsx
@@ -0,0 +1,81 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+/*
+Consuma a API e liste todos o pokemons da consulta do seguinte endpoint. 
+https://pokeapi.co/api/v2/pokemon
+
+Você deve exibir, de cada pokémon:
+- imagem
+- nome
+- experiência
+
+Você pode acessar as informações de cada pokemón individualmente em:
+https://pokeapi.co/api/v2/pokemon/:id
+
+
+DICA:
+imagem => sprites.front_default
+experiência => base_experience
+
+EXTRA: se puder ordene por nome.
+*/
+
+function App() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon").then((response) => {
+      const orderedPokemons = [...response.data.results];
+
+      orderedPokemons.sort((a, b) => {
+        if (a.name < b.name) {
+          return -1;
+        }
+
+        if (a.name > b.name) {
+          return 1;
+        }
+
+        return 0;
+      });
+
+      return setPokemons(orderedPokemons);
+    });
+  }, []);
+
+  return (
+    <>
+      <h3>desafio fernandev</h3>
+      <h1>consumir api pokémon</h1>
+
+      {pokemons.map((item, index) => (
+        <Pokemon data={item} id={index} />
+      ))}
+    </>
+  );
+}
+
+const Pokemon = ({ data }) => {
+  const [details, setDetails] = useState(null);
+
+  useEffect(() => {
+    axios.get(data.url).then((response) => setDetails(response.data));
+  }, []);
+
+
+
+  return (
+    <div >
+      <span>
+        <b>{details.name}</b> - EXP {details.base_experience}
+      </span>
+      <img
+        src={details.sprites.front_default}
+        style={{ width: 30, marginRight: 20 }}
+      />
+    </div>
+  );
+};
+
+export default App;
